97a15903ca56a57c33a0e96901eff81d88ebd819
Yuda Amrullah <yudamarullah3@gmail.com>
2023-12-05 14:59:34 +0800

    Add files via upload
    jadwal

---
+++ Profile.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Navbar from "../component/Navbar";
+const Kelas = () => {
+  return (
+    <>
+      <Navbar />
+      <div></div>
+    </>
+  );
+};
+
+export default Kelas;
