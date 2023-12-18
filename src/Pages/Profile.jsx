2d5b50bd86c6d8ae4e425b035e4917647d336503
flazer1234567u <yudamarullah3@gmail.com>
2023-12-18 12:18:26 +0800

    profile

--- src/Pages/Profile.jsx
+++ src/Pages/Profile.jsx
@@ -1,12 +1,135 @@
+// eslint-disable-next-line no-unused-vars
 import React from "react";
 import Navbar from "../component/Navbar";
-const Kelas = () => {
+const Profile = () => {
   return (
     <>
       <Navbar />
-      <div></div>
+      <div>
+        <div className="w-[1440px] h-[1024px] pl-[26px] pr-12 pt-[198px] pb-[39px] bg-white flex-col justify-end items-start gap-[30px] inline-flex">
+          <div className="w-[1366px] h-[264px] relative">
+            <div className="w-[1366px] h-[264px] left-0 top-0 absolute bg-blue-600 rounded-[20px] shadow" />
+            <img className="w-[198px] h-[198px] left-[34px] top-[33px] absolute rounded-full" src="" alt="Jokowidodo" />
+            <div className="w-[338px] h-[87px] left-[254px] top-[78px] absolute">
+              <div className="left-0 top-0 absolute text-center text-black text-[25px] font-bold">JOKOWI DODO</div>
+              <div className="left-0 top-[44px] absolute text-center text-black text-[15px]">NIP. 199802290918839 12 </div>
+              <div className="left-0 top-[69px] absolute text-center text-black text-[15px]">Pangkat : Pembina utama madya, golongan IV/d</div>
+            </div>
+          </div>
+          <div className="w-[1366px] h-[493px] relative">
+            <div className="w-[1366px] h-[493px] left-0 top-0 absolute bg-blue-600 rounded-[10px]" />
+            <div className="w-[172px] h-[33px] left-[12px] top-[11px] absolute">
+              <div className="w-[172px] h-[33px] left-0 top-0 absolute bg-slate-400 rounded-[20px]" />
+              <button className="left-[47px] top-[7px] absolute text-center text-white text-[15px]">Biodata Pribadi</button>
+              <div className="w-4 h-4 left-[27px] top-[7px] absolute">
+                <div className="w-3 h-[13.33px] left-[2px] top-[1.33px] absolute"></div>
+              </div>
+            </div>
+            <div className="w-[218px] h-[367px] left-[15px] top-[60px] absolute">
+              <div clas sName="w-[130px] h-[22px] left-0 top-0 absolute">
+                <div className="w-32 h-[18px] left-[2px] top-0 absolute">
+                  <div className="left-[16px] top-0 absolute text-center text-black text-[15px]">Biodata Pribadi</div>
+                  <div className="w-3 h-[13.33px] left-0 top-0 absolute"></div>
+                </div>
+              </div>
+              <div className="w-[107px] h-[37px] left-[22px] top-[29px] absolute">
+                <div className="left-0 top-0 absolute text-center text-white text-[15px]">Nama Lengkap</div>
+                <div className="left-0 top-[19px] absolute text-center text-black text-[15px]">Jokowi Dodo</div>
+              </div>
+              <div className="w-[156px] h-[37px] left-[22px] top-[77px] absolute">
+                <div className="left-0 top-[19px] absolute text-center text-black text-[15px]">199802290918839 12</div>
+                <div className="left-0 top-0 absolute text-center text-white text-[15px]">NIP</div>
+              </div>
+              <div className="w-[164px] h-[43px] left-[21px] top-[140px] absolute">
+                <div className="left-0 top-[25px] absolute text-center text-black text-[15px]">Surakarta, 21 Juni 1961</div>
+                <div className="left-0 top-0 absolute text-center text-white text-[15px]">Tempat Tanggal Lahir</div>
+              </div>
+              <div className="w-[134px] h-[43px] left-[20px] top-[200px] absolute">
+                <div className="left-0 top-[25px] absolute text-center text-black text-[15px]">123456789XXXX</div>
+                <div className="left-0 top-0 absolute text-center text-white text-[15px]">Nomor Telepon/Hp</div>
+              </div>
+              <div className="w-[200px] h-[43px] left-[18px] top-[257px] absolute">
+                <div className="left-0 top-[25px] absolute text-center text-black text-[15px]">Jokowidodo.gru@gmail.com</div>
+                <div className="left-[2px] top-0 absolute text-center text-white text-[15px]">Email</div>
+              </div>
+              <div className="w-[141px] h-[43px] left-[22px] top-[324px] absolute">
+                <div className="left-0 top-[25px] absolute text-center text-black text-[15px]">Tidak memiliki BPJS</div>
+                <div className="left-0 top-0 absolute text-center text-white text-[15px]">Nomor BPJS</div>
+              </div>
+            </div>
+            <div className="w-[223px] h-[377px] left-[687px] top-[54px] absolute">
+              <div className="w-[212px] h-6 left-0 top-0 absolute">
+                <div className="left-[42px] top-[6px] absolute text-center text-black text-[15px]">Alamat Tempat Tinggal</div>
+              </div>
+              <div className="w-[177px] h-[351px] left-[46px] top-[26px] absolute">
+                <div className="w-[177px] h-[37px] left-0 top-0 absolute">
+                  <div className="left-[3px] top-0 absolute text-center text-white text-[15px]">Alamat Tinggal Saat Ini</div>
+                  <div className="left-0 top-[19px] absolute text-center text-black text-[15px]">Jalan Adi Sucipto No 168</div>
+                </div>
+                <div className="w-[115px] h-[37px] left-[3px] top-[58px] absolute">
+                  <div className="left-0 top-[19px] absolute text-center text-black text-[15px]">Karanganyar</div>
+                  <div className="left-0 top-0 absolute text-center text-white text-[15px]">Kabupaten/Kota</div>
+                </div>
+                <div className="w-28 h-[43px] left-[3px] top-[116px] absolute">
+                  <div className="left-0 top-[25px] absolute text-center text-black text-[15px]">Desa Blulukan</div>
+                  <div className="left-0 top-0 absolute text-center text-white text-[15px]">Kelurahan/Desa</div>
+                </div>
+                <div className="w-[79px] h-[43px] left-[3px] top-[180px] absolute">
+                  <div className="left-0 top-[25px] absolute text-center text-black text-[15px]">Colomadu</div>
+                  <div className="left-0 top-0 absolute text-center text-white text-[15px]">Kecamatan</div>
+                </div>
+                <div className="w-[63px] h-[43px] left-[3px] top-[244px] absolute">
+                  <div className="left-0 top-[25px] absolute text-center text-black text-[15px]">57174</div>
+                  <div className="left-0 top-0 absolute text-center text-white text-[15px]">Kodepos</div>
+                </div>
+                <div className="w-[141px] h-[43px] left-[3px] top-[308px] absolute">
+                  <div className="left-0 top-[25px] absolute text-center text-black text-[15px]">Tidak memiliki BPJS</div>
+                  <div className="left-0 top-0 absolute text-center text-white text-[15px]">Nomor BPJS</div>
+                </div>
+              </div>
+            </div>
+            <div className="w-[313px] h-[347px] left-[287px] top-[80px] absolute">
+              <div className="w-[234px] h-[23.94px] left-0 top-[72px] absolute">
+                <div className="left-[26px] top-[3px] absolute text-center text-black text-[15px]">Riwayat Pendidikan Terakhir</div>
+                <div className="w-[23.94px] h-[23.94px] left-0 top-0 absolute"></div>
+              </div>
+              <div className="w-[285px] h-[347px] left-[28px] top-0 absolute">
+                <div className="w-[177px] h-[37px] left-[1px] top-0 absolute">
+                  <div className="left-0 top-0 absolute text-center text-white text-[15px]">Golongan Darah</div>
+                  <div className="w-[177px] left-0 top-[19px] absolute text-black text-[15px] ">O</div>
+                </div>
+                <div className="w-[171px] h-[43px] left-0 top-[98px] absolute">
+                  <div className="left-0 top-[25px] absolute text-center text-black text-[15px]">SD Negeri 112 Tirtoyoso</div>
+                  <div className="left-0 top-0 absolute text-center text-white text-[15px]">Sekolah Dasar</div>
+                </div>
+                <div className="w-[169px] h-[39px] left-[1px] top-[166px] absolute">
+                  <div className="left-0 top-[21px] absolute text-center text-black text-[15px]">SMP Negeri 1 Surakarta</div>
+                  <div className="left-0 top-0 absolute text-center text-white text-[15px]">SLTP/Sederajat</div>
+                </div>
+                <div className="w-[132px] h-[39px] left-[1px] top-[230px] absolute">
+                  <div className="left-0 top-[21px] absolute text-center text-black text-[15px]">SMAN 6 Surakarta</div>
+                  <div className="left-0 top-0 absolute text-center text-white text-[15px]">SLTA/Sederajat</div>
+                </div>
+                <div className="w-[284px] h-[61px] left-[1px] top-[286px] absolute">
+                  <div className="left-0 top-[25px] absolute text-black text-[15px] ">
+                    Jurusan Kehutanan, Fakultas Kehutanan
+                    <br />
+                    Universitas Gadjah Mada
+                  </div>
+                  <div className="left-0 top-0 absolute text-center text-white text-[15px]">Sarjana (S1)</div>
+                </div>
+              </div>
+            </div>
+            <div className="w-[114px] h-[21px] left-[219px] top-[17px] absolute">
+              <div className="w-[114px] h-[21px] left-0 top-0 absolute">
+                <button className="left-[32px] top-0 absolute text-center text-white text-[15px]">Pengaturan</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
 
-export default Kelas;
+export default Profile;
